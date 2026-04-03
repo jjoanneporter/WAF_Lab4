5bb08c718d463170f6e496f452164ebf7d3ad931
jjoanneporter <jporter9@hawk.iit.edu>
2026-04-03 12:21:40 -0500

    fix bugs in part 1

--- jporter-lab4.js
+++ jporter-lab4.js
@@ -4,21 +4,22 @@
 (function() {
 
 // 1a. changing main headline
-document.querySelector('.hero h1').textContent =
+document.querySelector('section h1').textContent =
 "Uplift Your Brand with Stellar Marketing";
 
-// 1b. changing the subheadline text 
-document.querySelector('.hero p').innerHTML =
+// 1b. changing subheadline
+document.querySelector('section p').innerHTML =
 "<strong><em>Utilize cutting-edge strategies from Stellar Marketing to help your business thrive and excel.</em></strong>";
 
-// 1c. changing the hero background image
-document.querySelector('.hero').style.backgroundImage =
+// 1c. changing hero background
+document.querySelector('section').style.backgroundImage =
 "url('https://picsum.photos/id/683/1280/720')";
 
-// 1d. remove "get started" button
-document.querySelector('.hero a').remove();
+// 1d. remove button
+const btn = document.querySelector('section a');
+if (btn) btn.remove();
 
-// 1e. changing navbar background color to match footer
+// 1e. matching navbar color to the footer
 const footerColor = window.getComputedStyle(document.querySelector('footer')).backgroundColor;
 document.querySelector('nav').style.backgroundColor = footerColor;
 
